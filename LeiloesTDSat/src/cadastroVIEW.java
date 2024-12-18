49fc3eda1a2c0e91cd20f3abfcb28271f0ce0409
Wilson Klaen <wilson4059@gmail.com>
2024-12-18 20:27:52 -0300

    teste

--- LeiloesTDSat/src/cadastroVIEW.java
+++ LeiloesTDSat/src/cadastroVIEW.java
@@ -142,7 +142,6 @@ public class cadastroVIEW extends javax.swing.JFrame {
 
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
-
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
     private void btnProdutosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProdutosActionPerformed
