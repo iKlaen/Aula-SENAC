418feb9ebfc8261d3b8b78291edad661e0278822
Wilson Klaen <wilson4059@gmail.com>
2024-12-18 20:55:37 -0300

    Ajuste ProdutosDAO - implementando consulta SQL para retornar os produtos cadastrados no SQL

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -39,9 +39,37 @@ public class ProdutosDAO {
         }
     }
 }
-    public ArrayList<ProdutosDTO> listarProdutos(){
+public ArrayList<ProdutosDTO> listarProdutos() {
+    String sql = "SELECT * FROM produtos";
+    listagem.clear();
+    
+    try {
+        conn = new conectaDAO().connectDB(); // Conecta ao banco de dados
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
         
-        return listagem;
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+        }
+    } catch (SQLException erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+    } finally {
+        try {
+            if (resultset != null) resultset.close();
+            if (prep != null) prep.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+    
+    return listagem;
+}
+
 }
 
